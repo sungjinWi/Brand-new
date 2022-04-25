dd43a648d2d43b9ebcea7c718369a98cd30c7ffe
= <=>
2022-04-25 15:19:49 +0900

    sol : rsp professor solution

--- pookiemongame/solution.js
+++ pookiemongame/solution.js
@@ -1,6 +1,6 @@
 /*
 V2에서 할것
-1. 가위바위보 게임 만들기 
+1. 가위바위보 게임 만들기 012로 해서 뺸다음 나머지 케이스 나눠보기
 2. 이동상태와 전투상태 구분할것 (키보드 이동)
 3. 몬스터 만날 확률
 */
@@ -29,17 +29,20 @@ const context = canvas.getContext("2d");
 let rsp = document.getElementById("rspGame")
 
 // 가위바위보 관련
-let userRsp = "";
+let userWeapon = "";
+let monWeaponRate = 2;
+let monWeapon = "";
 let buttons = [];
-buttons = [...(document.getElementsByClassName("rspGame"))] 
+buttons = [...(document.getElementsByClassName("Weapons"))] 
 // getElementsByClassName은 array가 아니다 => Array.from 또는 ...으로 배열로 바꾸기 가능
+
+// monster 관련
+let meetMon = false;
 
 // player 관련
 const arcRadius = 15;
 let arcPosX = 20;
 let arcPosY = 20;
-let arcMvDirX = 1;
-let arcMvDirY = -1;
 let arcMvSpd = 40;
 
 let player = {
@@ -54,51 +57,105 @@ const tileRow = 10;
 let tiles; // 벽돌 전체
 
 // game start여부
-let startBool = false;
+let isMoving = true;
 
 // 키처리 함수 추가
-document.addEventListener("keydown", startKey);
 document.addEventListener("keydown", keyDownEventHandler);
 document.addEventListener("keyup", keyUpEventHandler);
 
-// 가위바위보 버튼 누를 시
-buttons.forEach((button)=> button.addEventListener("click",()=>{ // onclick 아니라 click
-    userRsp = button.value;
-    
-    console.log(userRsp);
-}))
+// 가위바위보 버튼 누를 시 // onclick 아니라 click
+buttons.forEach((button)=> button.addEventListener("click", isRspWin))
 
-function startKey(e)
+// 움직일 때마다 랜덤 확률
+function meetRate() 
 {
-    // 게임 시작 상태 아니면 시작
-    if(e.key === " " && !startBool){
-        startBool = true;
-        setInterval(update, 10);
+    meetMon = (Math.round(Math.random()*10)) < 2;
+    
+    if(meetMon) {
+        isMoving = false;
+        alert("met mon");
+    };
+}
+
+// 가바보 게임 승패 결정
+function isRspWin()
+{
+    userWeapon = this.value;
+    
+    monWeaponRate = Math.ceil(Math.random()*3);
+    switch(monWeaponRate) {
+        // case는 범위 설정 불가
+        case 1 :
+            monWeapon = "bawi";
+            break;
+        case 2 :
+            monWeapon = "gawi";
+            break;
+        case 3 :
+            monWeapon = "bo";
+            break;
     }
+    console.log(userWeapon);
+    console.log(monWeapon)
+    if(userWeapon == monWeapon) {
+        alert("비겼으니 retry")
+        return false;
+    }
+    switch (userWeapon) {
+        case "gawi" : 
+            if(monWeapon == "bo") {
+                alert("win");
+            }
+            else {
+                alert("lose");
+            }
+            break;
+        case "bawi" :
+            if(monWeapon == "gawi") {
+                alert("win");
+            }
+            else {
+                alert("lose");
+            }
+            break;
+        case "bo" :
+            if(monWeapon == "bawi") {
+                alert("win");
+            }
+            else {
+                alert("lose");
+            }
+    }
+    isMoving = true;
+    
 }
 
 function keyDownEventHandler(e) 
 {   
-   if(startBool)
+   if(isMoving)
    {
 
         if(e.key === "ArrowRight" && arcPosX < canvas.width - arcRadius - tileWidth)
         {
             // 플레이어를 오른쪽으로 이동
             arcPosX+= arcMvSpd;
+            meetRate();
         }
         else if(e.key === "ArrowLeft" && arcPosX - arcRadius - tileWidth > 0)
         {
             // 플레이어를 왼쪽으로 이동
             arcPosX -= arcMvSpd;
+            meetRate();
         }
         else if(e.key === "ArrowUp" && arcPosY - arcRadius - tileHeight > 0)
         {
             arcPosY -= arcMvSpd;
+            meetRate();
         }
         else if(e.key === "ArrowDown" && arcPosY < canvas.width - arcRadius - tileHeight)
         {
             arcPosY += arcMvSpd;
+            meetRate();
         }
    }
 }
@@ -119,9 +176,6 @@ function update()
     player.right = arcPosX + arcRadius;
     player.top = arcPosY - arcRadius;
     player.bottom = arcPosY + arcRadius;
-
-    // 충돌 확인
-
 }
 
 function checkToWin()
@@ -130,10 +184,7 @@ function checkToWin()
             location.reload();
             alert("clear")
         }
-        console.log(isCollisionRectToRect(player, exit))
     }
-
-
 function isCollisionRectToRect(rectA,rectB)
 {
     //a의 왼쪽과 b의 오른쪽
@@ -171,7 +222,6 @@ function drawCanvas()
     context.rect(0, 0, 400, 400); 
     context.fillStyle = "lightgray";
     context.fill();
-    
     
     context.closePath();
 }
@@ -238,7 +288,7 @@ function setExit()
     )
 }
 
-
 setTiles();
 setExit();
 setInterval(draw, 10);
+setInterval(update, 10)
