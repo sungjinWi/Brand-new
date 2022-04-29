70d7488cdf55be2b79c24e636ad8163059cbad5d
= <=>
2022-04-29 11:15:41 +0900

    feat : validate block with nonce

--- 0428/block.js
+++ 0428/block.js
@@ -11,12 +11,14 @@
 import CryptoJS from "crypto-js"
 
 class Block {
-    constructor(index, data, timestamp, hash, previousHash) {
+    constructor(index, data, timestamp, hash, previousHash, difficulty, nonce) {
         this.index = index;
         this.data = data;
         this.timestamp = timestamp;
         this.hash = hash;
         this.previousHash = previousHash;
+        this.difficulty = difficulty;
+        this.nonce = nonce;
     }
 }
 
@@ -25,16 +27,20 @@ const getBlocks = () => { // 외부에 노출할 수 있게
     return blocks;
 }
 
-const calculateHash = (index, data, timestamp, previousHash) => {
-    return CryptoJS.SHA256(`${index + data + timestamp + previousHash}`).toString(); // toString() 한 것이 우리가 원하는 형태
+const calculateHash = (index, data, timestamp, previousHash, difficulty, nonce) => {
+    return CryptoJS.SHA256(`${index + data + timestamp + previousHash + difficulty + nonce}`).toString(); // toString() 한 것이 우리가 원하는 형태
 }
 
+// 0 하나로 시작하는 hash값을 만드는 매개변수(nonce)를 찾는다
+// 0 두개로 시작하는 hash값을 만드는 매개변수(nonce)를 찾는다 -> 하나만 값이 바뀌어도 hash는 완전히 바뀌기 때문에 난이도는 훨씬 상승
+// 16진수 64자리
+// 16진수 1자리 -> 2진수 4자리 256개의 0과 1로 표현
 
 const createGenesisBlock = () => {
-    const genesisBlock = new Block(0, 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks',new Date().getTime() / 1000, 0, 0);
+    const genesisBlock = new Block(0, 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks',new Date().getTime() / 1000, 0, 0, 0, 0);
     
-    genesisBlock.hash = calculateHash(genesisBlock.index, genesisBlock.data, 
-    genesisBlock.timestamp, genesisBlock.previousHash);
+    genesisBlock.hash = calculateHash(genesisBlock.index, genesisBlock.data, genesisBlock.timestamp,
+    genesisBlock.previousHash, genesisBlock.difficulty, genesisBlock.nonce);
     return genesisBlock;
 }
 
@@ -42,8 +48,13 @@ const createBlock = (blockData) => {
     const previousBlock = blocks[blocks.length - 1];
     const nextIndex = previousBlock.index + 1;
     const nextTimestamp = new Date().getTime() / 1000;
-    const nextHash = calculateHash(nextIndex, blockData ,nextTimestamp, previousBlock.hash);
-    const newBlock = new Block(nextIndex, blockData, nextTimestamp, nextHash, previousBlock.hash);
+    const nextDifficulty = 1;
+    const nextNonce = findNonce(nextIndex, blockData ,nextTimestamp, previousBlock.hash, nextDifficulty);
+
+    const nextHash = calculateHash(nextIndex, blockData ,nextTimestamp, previousBlock.hash, nextDifficulty, nextNonce);
+
+    const newBlock = new Block(nextIndex, blockData, nextTimestamp, nextHash, previousBlock.hash, nextDifficulty, nextNonce);
+
     if(isValidNewBlock(newBlock,previousBlock)){
         blocks.push(newBlock)
         return newBlock;
@@ -67,6 +78,8 @@ const isValidBlockStructure = (newBlock) => {
         && typeof (newBlock.timestamp) === "number"
         && typeof (newBlock.hash) === "string"
         && typeof (newBlock.previousHash) === "string"
+        && typeof (newBlock.difficulty) === "number"
+        && typeof (newBlock.nonce) === "number"
     )
     
 }
@@ -87,8 +100,52 @@ const isValidNewBlock = (newBlock, previousBlock) => {
     return true;
 }
 
+// 문제 해결을 검사하는 함수
+const hashMatchDifficulty = (hash, difficulty) => {
+    const binaryHash = hexToBinary(hash);
+    const requiredPrefix = '0'.repeat(difficulty);
+    // binaryHash가 requiredPrefix로 시작을 하는지 비교할 것이다
+
+    return binaryHash.startsWith(requiredPrefix);
+}
+
+const hexToBinary = (hex) => {
+    const lookupTable = {
+        '0' : "0000", '1' : "0001", '2' : "0010", '3' : "0011", 
+        '4' : "0100", '5' : "0101", '6' : "0110", '7' : "0111", 
+        '8' : "1000", '9' : "1001", 'a' : "1010", 'b' : "1011", 
+        'c' : "1100", 'd' : "1101", 'e' : "1110", 'f' : "1111"
+    }
+
+    let binary = '';
+    for(let i = 0; i < hex.length; i++)
+    {
+        if(lookupTable[hex[i]]) {
+            binary += lookupTable[hex[i]];
+        }
+        else {
+            console.log("invalid hex : ", hex);
+            return null;
+        }
+    }
+    return binary;
+}
+
+const findNonce = (index, data, timestamp, previousHash, difficulty) => {
+    let nonce = 0;
+
+    while(true)
+    {
+        let hash = calculateHash(index, data, timestamp, previousHash, difficulty, nonce);
+
+        if(hashMatchDifficulty(hash, difficulty)) {
+            return nonce;
+        }
+        nonce++;
+    }
+
+}
+
 const blocks = [createGenesisBlock()];
-
-
 
 export { getBlocks , createBlock, blocks };
