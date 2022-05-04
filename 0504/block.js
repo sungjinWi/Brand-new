8b4fa78d77bc0e8afb3677a37b243835410a05ec
= <=>
2022-05-04 09:38:09 +0900

    0504 update

---
+++ 0504/block.js
@@ -0,0 +1,160 @@
+// blockchain 관련 function
+// block 구조 설계
+/*
+    index : 블록체인의 높이
+    data : 블록에 포함된 모든 데이터 (트랜잭션 포함)
+    timestamp : 블록이 생성된 시간
+    hash : 블록 내부 데이터로 생성한 sha256 값 (블록의 유일성)
+    previousHash : 이전 블록의 해쉬 (이전 블록 참조)
+*/
+
+import CryptoJS from "crypto-js"
+
+class Block {
+    constructor(index, data, timestamp, hash, previousHash, difficulty, nonce) {
+        this.index = index;
+        this.data = data;
+        this.timestamp = timestamp;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.difficulty = difficulty;
+        this.nonce = nonce;
+    }
+}
+
+
+const getBlocks = () => { // 외부에 노출할 수 있게
+    return blocks;
+}
+
+const getLatestBlock = () => {
+    return blocks[blocks.length - 1];
+}
+
+const calculateHash = (index, data, timestamp, previousHash, difficulty, nonce) => {
+    return CryptoJS.SHA256(`${index + data + timestamp + previousHash + difficulty + nonce}`).toString(); // toString() 한 것이 우리가 원하는 형태
+}
+
+// 0 하나로 시작하는 hash값을 만드는 매개변수(nonce)를 찾는다
+// 0 두개로 시작하는 hash값을 만드는 매개변수(nonce)를 찾는다 -> 하나만 값이 바뀌어도 hash는 완전히 바뀌기 때문에 난이도는 훨씬 상승
+// 16진수 64자리
+// 16진수 1자리 -> 2진수 4자리 256개의 0과 1로 표현
+
+// new Date().getTime() / 1000
+const createGenesisBlock = () => {
+    const genesisBlock = new Block(0, 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks',0, 0, 0, 0, 0);
+    
+    genesisBlock.hash = calculateHash(genesisBlock.index, genesisBlock.data, genesisBlock.timestamp,
+    genesisBlock.previousHash, genesisBlock.difficulty, genesisBlock.nonce);
+    return genesisBlock;
+}
+
+const createBlock = (blockData) => {
+    const previousBlock = blocks[blocks.length - 1];
+    const nextIndex = previousBlock.index + 1;
+    const nextTimestamp = new Date().getTime() / 1000;
+    const nextDifficulty = 1;
+    const nextNonce = findNonce(nextIndex, blockData ,nextTimestamp, previousBlock.hash, nextDifficulty);
+
+    const nextHash = calculateHash(nextIndex, blockData ,nextTimestamp, previousBlock.hash, nextDifficulty, nextNonce);
+
+    const newBlock = new Block(nextIndex, blockData, nextTimestamp, nextHash, previousBlock.hash, nextDifficulty, nextNonce);
+
+        return newBlock;
+}
+
+const addBlock = (newBlock,previousBlock) => {
+    if(isValidNewBlock(newBlock,previousBlock)){
+        blocks.push(newBlock)
+        return true;
+    }
+    return false
+}
+
+
+
+//블록의 무결성 검증
+/*
+    블록의 인덱스가 이전 블록 인덱스보다 1크다
+    블록의 previousHash가 이전 블록의 hash이다
+    블록의 구조가 일치해야 한다.
+*/
+
+const isValidBlockStructure = (newBlock) => {
+    return(
+        typeof (newBlock.index) === "number"
+        && typeof (newBlock.data) === "string"
+        && typeof (newBlock.timestamp) === "number"
+        && typeof (newBlock.hash) === "string"
+        && typeof (newBlock.previousHash) === "string"
+        && typeof (newBlock.difficulty) === "number"
+        && typeof (newBlock.nonce) === "number"
+    )
+    
+}
+
+const isValidNewBlock = (newBlock, previousBlock) => {
+    if (newBlock.index !== previousBlock.index + 1) {
+        console.log("invalid index");
+        return false;
+    }
+    else if (newBlock.previousHash !== previousBlock.hash) {
+        console.log("invalid previous hash");
+        return false;
+    }
+    else if (!isValidBlockStructure(newBlock)) {
+        console.log("invalid block structure");
+        return false;
+    }
+    return true;
+}
+
+// 문제 해결을 검사하는 함수
+const hashMatchDifficulty = (hash, difficulty) => {
+    const binaryHash = hexToBinary(hash);
+    const requiredPrefix = '0'.repeat(difficulty);
+    // binaryHash가 requiredPrefix로 시작을 하는지 비교할 것이다
+
+    return binaryHash.startsWith(requiredPrefix);
+}
+
+const hexToBinary = (hex) => {
+    const lookupTable = {
+        '0' : "0000", '1' : "0001", '2' : "0010", '3' : "0011", 
+        '4' : "0100", '5' : "0101", '6' : "0110", '7' : "0111", 
+        '8' : "1000", '9' : "1001", 'a' : "1010", 'b' : "1011", 
+        'c' : "1100", 'd' : "1101", 'e' : "1110", 'f' : "1111"
+    }
+
+    let binary = '';
+    for(let i = 0; i < hex.length; i++)
+    {
+        if(lookupTable[hex[i]]) {
+            binary += lookupTable[hex[i]];
+        }
+        else {
+            console.log("invalid hex : ", hex);
+            return null;
+        }
+    }
+    return binary;
+}
+
+const findNonce = (index, data, timestamp, previousHash, difficulty) => {
+    let nonce = 0;
+
+    while(true)
+    {
+        let hash = calculateHash(index, data, timestamp, previousHash, difficulty, nonce);
+
+        if(hashMatchDifficulty(hash, difficulty)) {
+            return nonce;
+        }
+        nonce++;
+    }
+
+}
+
+let blocks = [createGenesisBlock()];
+
+export { getBlocks , getLatestBlock , createBlock ,addBlock , isValidNewBlock ,blocks };
---
+++ 0504/p2pServer.js
@@ -0,0 +1,159 @@
+// peer to peer
+// 다른 노드와 통신을 위한 서버
+
+import WebSocket from "ws";
+// import random from "random";
+import { WebSocketServer } from "ws";
+import { getBlocks, getLatestBlock, addBlock ,createBlock , isValidNewBlock , blocks } from "./block.js"
+
+const MessageType = {
+    // RESPONSE_MESSAGE : 0,
+    // SENT_MESSAGE : 1
+
+    // 최신 블록 요청
+    QUERY_LATEST : 0,
+
+    // 모든 블록 요청
+    QUERY_ALL : 1,
+
+    // 블록 전달
+    RESPONSE_BLOCKCHAIN: 2
+}
+
+const sockets = []; //배열 시작점의 주소를 sockets에 저장
+
+
+// 응답을 받는 서버의 역할
+const initP2PServer = (p2pPort) => {
+    const server = new WebSocketServer({port:p2pPort});
+    server.on("connection", (ws) => {
+        console.log("someone ws connected to me")
+        initConnection(ws); // 추가 안하면 message handle 못함
+    })
+    
+    
+    console.log("listening P2PServer Port : ", p2pPort)
+}
+
+const initConnection = (ws) => {
+    sockets.push(ws);
+    initMessageHandler(ws);
+
+    write(ws, queryAllMessage()); // 연결될 때 
+}
+
+const connectionToPeer = (newPeer) => {
+    const ws = new WebSocket(newPeer);
+    ws.on("open", () => {
+        initConnection(ws);
+        console.log("Connect peer : ", newPeer);
+    })
+    ws.on("error", () => {console.log("Fail to Connection peer : ", newPeer);})
+}
+
+const initMessageHandler = (ws) => {
+    ws.on("message", (data) => {
+        const message = JSON.parse(data);
+        switch(message.type)
+        {
+            case MessageType.QUERY_LATEST:
+                console.log("reply latest")
+                break;
+            case MessageType.QUERY_ALL:
+                write(ws, responseAllMessage())
+                break;
+            case MessageType.RESPONSE_BLOCKCHAIN:
+                console.log(ws._socket.remoteAddress, " : ", JSON.parse(message.data) )
+                replaceBlockchain(JSON.parse(message.data));
+                break;
+        }
+    })
+}
+
+const isValidBlockchain = (receiveBlockchain) => {
+    // 같은 제네시스 블록인가
+    if (JSON.stringify(receiveBlockchain[0]) !== JSON.stringify(getBlocks()[0])) 
+        return false;
+    
+    // 체인 내의 모든 블록을 확인
+    for (let i = 1; i < receiveBlockchain.length; i++) 
+    {
+        if (isValidNewBlock(receiveBlockchain[i],receiveBlockchain[i-1]) == false)
+            return false;
+        
+    }
+
+    return true;
+}
+
+const replaceBlockchain = (receiveBlockchain) => {
+
+
+    if(isValidBlockchain(receiveBlockchain))
+    {
+        if(receiveBlockchain.length > blocks.length)
+        {
+            console.log("받은 블록체인 길이가 길다")
+            blocks = receiveBlockchain;
+        }
+        else if (receiveBlockchain.length == blocks.length && random.boolean()) // random을 조건문에 넣어주기 
+        {
+            console.log("받은 블록체인 길이가 같다")
+            blocks = receiveBlockchain;
+
+        }
+    }
+    else{
+        console.log("받은 블록체인에 문제")
+    }
+}
+
+
+const queryLatestMessage = () => {
+    return ({
+            "type":MessageType.QUERY_LATEST,
+            "data":null
+    })
+}
+const queryAllMessage = () => {
+    return ({
+            "type":MessageType.QUERY_ALL,
+            "data":null
+    })
+}
+const responseLatestMessage = () => {
+    return ({
+            "type":MessageType.RESPONSE_BLOCKCHAIN ,
+            "data":JSON.stringify(getLatestBlock()) // 이렇게 보내면 JSON.parse써서 데이터 가공해야함 
+            // "data":getLatestBlock() // 요렇게 보내면 바로 쓰기 편하다 하지만 데이터가 커질수록 낭비되는 패킷이 많아진다
+    })
+}
+const responseAllMessage = () => {
+    return ({
+            "type":MessageType.RESPONSE_BLOCKCHAIN ,
+            "data":JSON.stringify(getBlocks())
+            // "data":getBlocks()
+    })
+}
+
+const write = (ws, message) => { // 보낼 상대방의 ws
+    console.log("write()", message);
+    ws.send(JSON.stringify(message));
+}
+
+const broadcasting = (message) => {
+    sockets.forEach((socket)=>{
+        write(socket, message)
+    });
+}
+
+// 내가 새로운 블록을 채굴했을 때 다른 사람들에게 전파
+const mineBlock = (blockData) => {
+    const newBlock = createBlock(blockData);
+    if(addBlock(newBlock,getLatestBlock()))
+    {
+        broadcasting(responseLatestMessage())
+    }
+}
+
+export { initP2PServer ,connectionToPeer, broadcasting, responseLatestMessage, mineBlock};
