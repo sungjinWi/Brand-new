0f80034cbc36cb1b94c6950927b6a0dfeb1226f9
sungjinWi <rkdlem94@gmail.com>
2022-05-10 09:13:20 +0900

    Feat : ready for 0510

---
+++ 0510/main2.js
@@ -0,0 +1,15 @@
+// p2p 서버 초기화
+// http 서버 초기화
+// blockchain 함수 사용
+
+import {initHttpServer} from "./httpServer.js"; // import 할 때 .js 까지
+import { initP2PServer } from "./p2pServer.js";
+import { initWallet } from "./wallet.js";
+
+const httpPort = parseInt(process.env.HTTP_PORT) || 3002; 
+const p2pPort = parseInt(process.env.HTTP_PORT) || 6002; 
+// 직접 숫자를 집어넣는 것은 programmer만 vscode 켜서 할 수 있다 하지만 .env파일로 해놓으면 외부에서 조종 할 수 있음
+
+initWallet();
+initHttpServer(httpPort);
+initP2PServer(p2pPort);
